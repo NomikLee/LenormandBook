e121512ae75512123cf1bdf9ab76c560076b79dc
Nomik Lee <s79318@icloud.com>
2023-07-13 16:05:08 +0800

    Initial Commit

---
+++ LenormandBook/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  LenormandBook
+//
+//  Created by Pinocchio on 2023/7/13.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
